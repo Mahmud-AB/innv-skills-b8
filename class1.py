e720008282a5e376a3baa38078e11efd53d8ca8d
Abdullah Mahmud <104511004+Mahmud-AB@users.noreply.github.com>
2025-03-10 22:00:50 +0600

    batch 8

---
+++ class1.py
@@ -0,0 +1,172 @@
+# x = 10
+# print(x)
+# x = 12
+# print("Address of variable",id(x))
+# print(x)
+
+#varible rules
+#1 variable name shouldn't start with number,special character
+# 1x = 10,@x = 10 
+#2 no -  and space in variable name,
+# my-name = abdullah, my name = abdullah
+#3 variable name  shouldn't end with special character
+# x@ = 10
+#4 variable name should not  be keyword
+# object = 10,list,string
+
+# #variable name should be meaningful
+# student_age = "abdullah"
+
+# #snake case
+# student_age = "abdullah"  #variable name, function name
+#pascal case
+# StudentAge #class name
+
+#int number data type
+# x=10
+# print(x)
+# print(type(x))
+# #float number data type but decimal point
+# y=10.5
+# print(y)
+# print(type(y))
+# #string word/sentence data type
+# name = "abdullah"
+# print(name)
+# print(type(name))
+# #bool True/False data type
+# z = True
+# print(z)
+# print(type(z))
+#structured  data type
+#list, tuple, set, dictionary
+#list
+# list_1 = [10,20,30,40,50]
+# print(list_1)
+# print(type(list_1))
+# #tuple
+# tuple_1 = (10,20,30,40,50)
+# print(tuple_1)
+# print(type(tuple_1))
+# #set
+# set_1 = {10,20,30,40,50}
+# print(set_1)
+# print(type(set_1))
+
+# #dictionary  key:value pair
+# dict_1 = {"name":"abdullah","age":25}
+# print(dict_1)
+# print(type(dict_1))
+
+# #arithmetic operator
+# #  +, -, *, /, %, //, **
+# x = 10 + 10
+# print("+",x)
+# y = 10 - 5
+# print("-",y)
+# z = 10 * 10
+# print("*",z)
+# a = 10 / 2
+# print("/",a)
+# b = 10 % 3
+# print("%",b)
+# c = 10 // 3
+# print("//",c)
+# d = 10 ** 2
+# print("**",d)
+
+# #comparison operator
+# # <, >, <=, >=, ==, !=
+# x = 10 < 20
+# print("<",x)
+# y = 10 > 20
+# print(">",y)
+# z = 10 <= 20
+# print("<=",z)
+# a = 10 >= 20
+# print(">=",a)
+# b = 10 == 20  
+# print("==",b)
+# c = 10 != 20
+# print("!=",c)
+
+# a =  10 #int
+# b = "10" #string
+
+# print(type(a),a)
+# print(type(b),b)
+# c = a == b
+# print(c)
+
+# print("=====================")
+# a = b #string pass
+# print("=====================")
+
+# print(type(a),a)
+# print(type(b),b)
+# c = a == b
+# print(c)
+
+# #logical operator
+# # and, or, not
+# x = 10
+# y = 20
+# z = 30
+# a = x < y and y > z or x < z #True and False
+# print("and",a)
+# b = x < y or y > z #True or False
+# print("or",b)
+# c = not x < y
+# print("not",c)
+
+# #assignment operator
+# # =, +=, -=, *=, /=, %=, //=, **=
+
+# x = 10
+# print(x)
+# y = 20
+# x += 10 - 5 + y #x = x + 10 - 5 + y == x += 10
+# print(x)
+# x -= 10
+# print(x)
+# x *= 10
+# print(x)
+# x /= 10
+# print(x)
+# x %= 10
+# print(x)
+# x //= 10
+# print(x)
+# x **= 10
+# print(x)
+
+# a = 10
+# b = a
+# b = 20
+
+# print(a)
+# print(b)
+
+# list1 = [1,2]
+# list2 = list1
+
+# list2.append(3)
+
+# print("list1",list1)
+# print("list2",list2)
+# print(id(list1))
+# print(id(list2))
+
+# name = input("Enter your name:")
+# age = int(input("Enter your age:"))
+# print(name)
+# print(type(name))
+# print(age)
+# print(type(age))
+
+
+# Ask the user to enter the following details:
+# Name, Age,roll number, marks(3sub)
+# print the following details
+# Name, Age, roll number, marks (in different lines)
+#calculate the totall  marks,avg
---
+++ test.py
@@ -0,0 +1,2 @@
+print("hello world")
+print(list(range(10)))
